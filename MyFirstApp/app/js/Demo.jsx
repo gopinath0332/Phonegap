574efd98390f408898a6b25881dbbacef3cb7f33
gopinath0332 <gopinath0332@outlook.com>
2016-07-31 22:18:59 +0530

    Adding static reactjs appbase

--- MyFirstApp/app/js/Demo.jsx
+++ MyFirstApp/app/js/Demo.jsx
@@ -1,7 +1,31 @@
 const $ = require("jquery");
-const React = require("react");
-const ReactDOM = require("react-dom");
-const [targetNode] = $("#content")
+// const React = require("react");
+// const ReactDOM = require("react-dom");
+// const [targetNode] = $("#content");
+var targetNode = $("#content")[0];
+
+var Counter = React.createClass({
+	getInitialState: function(){
+		return{"totalTime":60000,"elapsed":0,"interval":1000,"timeLeft":1};
+	},
+	_startCounter: function(op){
+		var context = this;
+		if(this.state.timeLeft){
+			this.setState({"elapsed":this.state.elapsed+this.state.interval});
+			this.setState({"timeLeft":this.state.totalTime-this.state.elapsed});
+			console.log(this.state);
+			setTimeout(this._startCounter,this.state.interval);
+		}
+	},
+	render: function(){
+		return(<div>
+			<button onClick={this._startCounter} >Press Me!</button>
+			<div>Total Time: {this.state.totalTime/1000} seconds</div>
+			<div>Remaing Time: {this.state.timeLeft ==1 ? 0: (this.state.timeLeft/1000) } seconds</div>
+		</div>);
+	}
+});
+
 
 ReactDOM.render(
-    <h1>React + PhoneGap App Demo</h1>, targetNode);
+   <Counter></Counter>, targetNode);
