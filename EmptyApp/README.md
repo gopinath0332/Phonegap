2b2567cd0b4df11a54af2aaf1e45c89c19743bc8
gopinath0332 <gopinath0332@outlook.com>
2016-08-16 22:46:37 +0530

    Adding Empty react app

---
+++ EmptyApp/README.md
@@ -0,0 +1,3 @@
+Empty App 
+
+React + webpack + phonegap
---
+++ EmptyApp/app/app.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// 
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+
+import $ from "jquery";
+
+const [targetNode] = $("#app");
+
+ReactDOM.render(<h1>Empty React APP!</h1>, targetNode);
